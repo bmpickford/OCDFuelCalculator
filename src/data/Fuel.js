48bf98cfe081bce35996db881e81523cd9c0b1b5
bmpickford <b.mpickford@outlook.com>
2020-01-26 19:21:02 +1000

    Make comparison include types

--- src/data/Fuel.js
+++ src/data/Fuel.js
@@ -1,5 +1,5 @@
 import prices from './fuel.json';
 
 export const FindLitresForFuelPrice = (fuelPrice) => {
-    return prices.find(p => p.price == fuelPrice);
+    return prices.find(p => parseFloat(p.price).toFixed(2) === parseFloat(fuelPrice).toFixed(2));
 }
